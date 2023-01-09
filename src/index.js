310a7bbbc7015ddf22a48d57afd7b2484ad9f9a2
vibhuti1 <gajinkar.vibhuti@gmail.com>
2023-01-09 22:05:29 +1000

    Sample react app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
 
-/* GLOBAL VARIABLES */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-window.$primaryLanguage = 'en';
-window.$secondaryLanguage = 'pl';
-window.$primaryLanguageIconId = 'primary-lang-icon';
-window.$secondaryLanguageIconId = 'secondary-lang-icon';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
