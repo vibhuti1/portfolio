ce2c829f7567aa6214b29543ebc25a58e03af977
vibhuti1 <gajinkar.vibhuti@gmail.com>
2023-01-16 23:53:22 +1000

    Path update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />
